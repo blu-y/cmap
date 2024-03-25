474dc4fee6aeef0f08c108d0e64569a6dfa39deb
blu-y <a_o@kakao.com>
2024-03-26 03:49:07 +0900

    0325

--- downloadmodel.sh
+++ downloadmodel.sh
@@ -1,9 +1,3 @@
-cd ViT-B-16-SigLIP
-wget https://huggingface.co/timm/ViT-B-16-SigLIP/resolve/main/open_clip_pytorch_model.bin
-cd ..
-cd Vit-B-32-256
-wget https://huggingface.co/laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K/resolve/main/open_clip_pytorch_model.bin
-cd ..
-cd Vit-B-32
-wget https://huggingface.co/laion/CLIP-ViT-B-32-DataComp.XL-s13B-b90K/resolve/main/open_clip_pytorch_model.bin
-cd ..
+wget https://huggingface.co/timm/ViT-B-16-SigLIP/resolve/main/open_clip_pytorch_model.bin -O ./ViT-B-16-SigLIP/open_clip_pytorch_model.bin
+wget https://huggingface.co/laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K/resolve/main/open_clip_pytorch_model.bin -O ./ViT-B-32-256/open_clip_pytorch_model.bin
+wget https://huggingface.co/laion/CLIP-ViT-B-32-DataComp.XL-s13B-b90K/resolve/main/open_clip_pytorch_model.bin -O ./ViT-B-32/open_clip_pytorch_model.bin
