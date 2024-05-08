09251acfb735295a449313f69fbd8140cd8366fb
blu-y <a_o@kakao.com>
2024-05-08 17:29:50 +0900

    0508

--- downloadmodel.sh
+++ downloadmodel.sh
@@ -1,6 +1,5 @@
 wget https://huggingface.co/timm/ViT-B-16-SigLIP/resolve/main/open_clip_pytorch_model.bin -O ./ViT-B-16-SigLIP/open_clip_pytorch_model.bin
 wget https://huggingface.co/laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K/resolve/main/open_clip_pytorch_model.bin -O ./ViT-B-32-256/open_clip_pytorch_model.bin
 wget https://huggingface.co/laion/CLIP-ViT-B-32-DataComp.XL-s13B-b90K/resolve/main/open_clip_pytorch_model.bin -O ./ViT-B-32/open_clip_pytorch_model.bin
-wget https://huggingface.co/laion/CLIP-ViT-L-14-DataComp.XL-s13B-b90K/resolve/main/open_clip_pytorch_model.bin -O ./ViT-L-14/open_clip_pytorch_model.bin
 wget https://huggingface.co/apple/DFN2B-CLIP-ViT-L-14/resolve/main/open_clip_pytorch_model.bin -O ./ViT-L-14-DFN2B/open_clip_pytorch_model.bin
 wget https://huggingface.co/apple/DFN5B-CLIP-ViT-H-14/resolve/main/open_clip_pytorch_model.bin -O ./ViT-H-14-DFN5B/open_clip_pytorch_model.bin
