5e1541d68de0ecb10f7bcc7843b07a54000f80ef
blu-y <102652794+blu-y@users.noreply.github.com>
2024-05-29 21:22:20 +0900

    Update jetson_setup.sh

--- jetson_setup.sh
+++ jetson_setup.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install pv vim -y
 sudo vi /etc/apt/sources.list +%s/ports.ubuntu.com/ftp.kaist.ac.kr +wq!
 sudo apt update
-sudo apt install python3-pip libopenblas-dev -y
+sudo apt install python3-pip libopenblas-dev axel -y
 pip install --upgrade pip
 sudo -H pip install -U jetson-stats
 export TORCH_INSTALL=https://developer.download.nvidia.cn/compute/redist/jp/v60/pytorch/torch-2.4.0a0+07cecf4168.nv24.05.14710581-cp310-cp310-linux_aarch64.whl
@@ -12,7 +12,7 @@ pip install numpy=='1.26.1'
 pip install --no-cache $TORCH_INSTALL
 echo "export LD_LIBRARY_PATH=/usr/lib/llvm-8/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
 echo "export PATH=/home/jetson/.local/bin:$PATH" >> ~/.bashrc
-
+source ~/.bashrc
 wget https://github.com/rustdesk/rustdesk/releases/download/1.2.3-2/rustdesk-1.2.3-2-aarch64.deb
 sudo dpkg -i rustdesk-1.2.3-2-aarch64.deb
 
@@ -23,7 +23,7 @@ cd cmap
 echo downloading datasets
 mkdir -p dataset/files
 cd dataset
-wget https://zenodo.org/records/7811795/files/Robot%40Home2_files.tgz
+axel https://zenodo.org/records/7811795/files/Robot%40Home2_files.tgz
 axel https://zenodo.org/records/7811795/files/Robot%40Home2_db.tgz
 md5sum Robot@Home2_db.tgz Robot@Home2_files.tgz
 echo d34fb44c01f31c87be8ab14e5ecd0767  Robot@Home2_db.tgz
